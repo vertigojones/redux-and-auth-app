6dbae28743933ed1a9f9d085b971873b97db6074
Owen Liversidge <owen@Tripwire.local>
2019-01-31 09:32:16 -0500

    user validation with jwt token now working

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://vertigojones:Conversations101@ds231360.mlab.com:31360/owen-devconnector"
+    "mongodb://vertigojones:Conversations101@ds231360.mlab.com:31360/owen-devconnector",
+  secretOrKey: "secret"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
 // Load User model
 const User = require("../../models/User");
@@ -63,12 +66,41 @@ router.post("/login", (req, res) => {
     // Check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        // User matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT payload
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
